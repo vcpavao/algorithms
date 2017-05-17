d8af4a02f5917eabf640fcbdc0fe7f00acae4a84
vcpavao <korgkeys13>
2017-05-16 22:34:01 -0400

    More additions to brute force

--- AlgPrac/src/BruteForcePrac.java
+++ AlgPrac/src/BruteForcePrac.java
@@ -19,18 +19,46 @@ public class BruteForcePrac {
 	}
 	
 	/**
+	 * Selection sort finds the smallest element in an array
+	 * and swaps it with the first nonsorted element in order
+	 * to produce a sorted array
+	 * @param values - given array of positive numbers
+	 */
+	public static void selectionSort(int [] values) {
+		for(int i = 0; i < (values.length - 2); i++) {
+			int min = i;
+			for(int j = i + 1; j < (values.length); j++) {
+				if(values[j] < values[min]) {
+					min = j;
+				}
+			}
+			int temp = values[min];
+			values[min] = values[i];
+			values[i] = temp;
+		}
+	}
+	
+	/**
 	 * 
 	 * @param key - number being searched
 	 * @param values - array of numbers
 	 * @return location of key if key is found, if not -1 is returned
 	 */
-	public static int exhaustive_search(int key, int [] values) {
+	public static int exhaustiveSearch(int key, int [] values) {
 		for(int k = 0; k < values.length; k++) {
 			if(values[k] == key) {
 				return k;
 			}
 		}
 		return -1;
+	}
+	
+	public static String printArray(int [] values) {
+		String s = "";
+		for(int l = 0; l < values.length; l++) {
+			s+= values[l] + ", ";
+		}
+		return s;
 	}
 	
 	public static void main(String[] args) {
@@ -47,8 +75,12 @@ public class BruteForcePrac {
 		for(int i = 0; i < size; i++) {
 			values[i] = (r.nextInt(99) + 1);
 		}
+		//Selection sort test
+		System.out.println("Non sorted array: " + printArray(values));
+		selectionSort(values);
+		System.out.println("Sorted array: " + printArray(values));
 		int search_key = 20;
 		System.out.println("Location of " + search_key +
-				" in array: " + exhaustive_search(search_key, values));
+				" in array: " + exhaustiveSearch(search_key, values));
 	}
 }
