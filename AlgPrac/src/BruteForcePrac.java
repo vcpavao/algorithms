74133fd42b3ba5f25c7529558cd4111ab1b9ef1d
vcpavao <korgkeys13>
2017-05-16 22:14:54 -0400

    Additions to BruteForce

--- AlgPrac/src/BruteForcePrac.java
+++ AlgPrac/src/BruteForcePrac.java
@@ -1,35 +1,53 @@
+import java.util.Random;
 
 public class BruteForcePrac {
 	/**
 	 * Brute force method for finding the integer square root of a value
-	 * @param n given value
+	 * @param n - given value
 	 * @return value's integer square root
 	 */
 	public static int square_root(int n) {
 		int i;
-		for(i = 0; i * i < n; i++) {
+		//The square root will never equal more than half of the given number
+		for(i = 0; i < n/2; i++) {
+			//First i * i value greater than n reached
+			if(i * i > n) {
+				return i - 1;
+			}
 		}
-		return i;
+		return -1;
 	}
 	
 	/**
-	 * Brute force method for finding two real roots of a quadratic equation,
-	 * assumes "smart user" who will input reasonable integer values
-	 * @param a coefficient of x^2 term
-	 * @param b coefficient of x term
-	 * @param c integer
-	 * @return array containing the two real roots given three coefficients
+	 * 
+	 * @param key - number being searched
+	 * @param values - array of numbers
+	 * @return location of key if key is found, if not -1 is returned
 	 */
-	public static double [] roots(double a, double b, double c) {
-		double bs = b * b;
-		double sub = 4 * a * c;
-		double [] roots = {(-b + (Math.sqrt(bs - sub)))/(2*a), (-b - (Math.sqrt(bs - sub)))/(2*a)};
-		return roots;
+	public static int exhaustive_search(int key, int [] values) {
+		for(int k = 0; k < values.length; k++) {
+			if(values[k] == key) {
+				return k;
+			}
+		}
+		return -1;
 	}
 	
 	public static void main(String[] args) {
-		double [] h = roots(1.0, -4.0, 4.0);
-		System.out.println(h[0]);
-		System.out.println(h[1]);
+		//Square root testing requires one positive value
+		int num = 64;
+		System.out.println("The square root of " + num +
+				" is: " + square_root(num));
+		//Exhaustive search testing populates array with
+		//random numbers from 1 to 100
+		int size = 50;
+		int [] values = new int[size];
+		Random r = new Random();
+		for(int i = 0; i < size; i++) {
+			values[i] = (r.nextInt(99) + 1);
+		}
+		int search_key = 20;
+		System.out.println("Location of " + search_key +
+				" in array: " + exhaustive_search(search_key, values));
 	}
 }
